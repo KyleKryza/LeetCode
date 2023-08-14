05035fda25737ecbc5e48f1ad2b9111561a4222b
KyleKryza <ximkyx@gmail.com>
2023-08-13 17:59:09 -0700

    Medium 53

---
+++ MEDIUM/53/MaxSubarray.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let max = nums[0];
+    let current = 0;
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] > 0){
+            current += nums[i];
+
+            if(current > max){
+                max = current;
+            }
+        }
+        else{
+            if(current + nums[i] < 0){
+                current = 0;
+            }
+            else{
+                current += nums[i];
+            }
+        }
+        if(nums[i] > max){
+            max = nums[i];
+        }
+        console.log("Current: " + current + "    Max: " + max);
+    }
+    return max;
+};
+
+console.log(maxSubArray([-2,-1]))
---
+++ MEDIUM/53/readme.md
@@ -0,0 +1,9 @@
+# MEDIUM 53
+
+## Question
+Maximum Subarray
+
+## Description
+Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+[Link to Problem](https://leetcode.com/problems/maximum-subarray/)
